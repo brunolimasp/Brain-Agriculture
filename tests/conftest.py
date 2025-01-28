6d8e6af2ab3930237231794de48a44789b562265
Bruno <brunoaugustosp@outlook.com.br>
2025-01-28 01:36:32 -0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,8 @@
-# tests/conftest.py
 import pytest
-from fastapi.testclient import TestClient
 from sqlmodel import SQLModel, Session, create_engine
 
-from app import app  # Importe seu app do FastAPI
-from src.database import get_session  # Função que você usa como Depends para a sessão
+
+from src.models.tables import Producer, Property, Crop
 
 
 @pytest.fixture(name="test_engine")
@@ -14,28 +12,7 @@ def fixture_test_engine():
     SQLModel.metadata.create_all(engine)
     return engine
 
-
 @pytest.fixture(name="session")
 def fixture_session(test_engine):
     with Session(test_engine) as session:
         yield session
-
-
-@pytest.fixture(name="client")
-def fixture_client(session):
-    """
-    Retorna um TestClient que utiliza a sessão de teste.
-    Isso permite que a aplicação utilize o banco em memória.
-    """
-
-    # Override da dependência get_session para usar nossa session de teste
-    def override_get_session():
-        yield session
-
-    app.dependency_overrides[get_session] = override_get_session
-
-    with TestClient(app) as client:
-        yield client
-
-    # Remove o override após os testes
-    app.dependency_overrides.clear()
